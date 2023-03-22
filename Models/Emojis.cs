9c9eef5fa6d3dc90d00503b2ebb53c06a43d2e2d
Hargita Benjamin <hargita.beni@gmail.com>
2023-03-22 21:38:28 +0100

    added emojis class

---
+++ Models/Emojis.cs
@@ -0,0 +1,9 @@
+﻿namespace specchat.Models
+{
+    public class Emojis
+    {
+        public string Code { get; }
+        public ApplicationUser User { get; set; }
+        public Message Message { get; set; }
+    }
+}
