4b4d4202fe2c892f93a184be72f0cc6221ac7ef7
Hargita Benjamin <hargita.beni@gmail.com>
2023-03-22 20:30:32 +0100

    Topic class added

---
+++ Models/Topic.cs
@@ -0,0 +1,20 @@
+﻿namespace specchat.Models
+{
+    public class Topic
+    {
+        public Guid Id { get; }
+        public string Content { get; set; }
+        public DateTime Time { get; set; }
+        public ApplicationUser User { get; set; }
+        public Topic MainThread { get; set; }
+        public override string ToString()
+        {
+            return $"{User.UserName}: {Content} at {Time}";
+        }
+        public Topic()
+        {
+            Id = Guid.NewGuid();
+        }
+
+    }
+}
