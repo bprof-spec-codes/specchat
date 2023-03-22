05eb6c189ded8f98535aea2cea58016d8abd1275
Hargita Benjamin <hargita.beni@gmail.com>
2023-03-22 20:18:52 +0100

    Chat class added

---
+++ Models/Chat.cs
@@ -0,0 +1,21 @@
+﻿namespace specchat.Models
+{
+    public class Chat
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+        public virtual ICollection<Topic> Topics { get; set; }
+        public override string ToString()
+        {
+            return $"{Id}: {Name}";
+        }
+
+        public Chat()
+        {
+            Id = Guid.NewGuid();
+            Topics = new HashSet<Topic>();
+            Users = new HashSet<ApplicationUser>();
+        }
+    }
+}
