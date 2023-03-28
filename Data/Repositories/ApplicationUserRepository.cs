d384e150db4ba787f6257088a6ba329f5676ebe0
Stiiii <48644228+Stiiii@users.noreply.github.com>
2023-03-28 12:24:51 +0200

    AppUserRepo done
    finished the repository

--- Data/Repositories/ApplicationUserRepository.cs
+++ Data/Repositories/ApplicationUserRepository.cs
@@ -27,22 +27,42 @@ namespace specchat.Data.Repositories
 
 		public void Delete(string id)
 		{
-			throw new NotImplementedException();
+			var usr = _context.Users.FirstOrDefault(t => t.Id == id); // Chat.Id (Guid) nem összehasonlítható id(string) -vel
+			if (usr == null)
+			{
+				throw new ArgumentException("There's no User with this id: " + id);
+			}
+
+			_context.Users.Remove(usr);
+			_context.SaveChanges();
 		}
 
 		public IEnumerable<ApplicationUser> Read()
 		{
-			throw new NotImplementedException();
+			return _context.Users;
 		}
 
 		public ApplicationUser Read(string id)
 		{
-			throw new NotImplementedException();
+			var user = _context.Users.FirstOrDefault(t => t.Id == id);
+			if (user == null)
+			{
+				throw new ArgumentException("There's no User with this id: " + id);
+			}
+			return user;
 		}
 
 		public void Update(ApplicationUser user)
 		{
-			throw new NotImplementedException();
+			var old = _context.Users.FirstOrDefault(t => t.Id == user.Id);
+			if (old == null)
+			{
+				throw new ArgumentException("There's no User with this id: " + user.Id);
+			}
+			old.Email = user.Email;
+			old.FirstName = user.FirstName;
+			old.LastName = user.LastName;
+			_context.SaveChanges();
 		}
 	}
 }
