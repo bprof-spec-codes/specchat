3c689aa2c2ecba0f84bd3703106a911375d25a93
Dávid <david.gyongyosi@majin.dev>
2023-04-27 21:24:11 +0200

    channels component list added

--- ClientApp/src/app/left-sidebar/left-sidebar.component.ts
+++ ClientApp/src/app/left-sidebar/left-sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../services/chat.service';
+import { Chat } from '../models/chat';
 
 @Component({
   selector: 'left-sidebar',
@@ -6,7 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./left-sidebar.component.css']
 })
 export class LeftSidebarComponent {
+  chats?: Chat[] ;
   isExpanded = false;
+
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit(): void {
+    this.chatService.getChat().subscribe((result: Chat[]) => {
+      this.chats = result;
+    });
+  }
 
   collapse() {
     this.isExpanded = false;
