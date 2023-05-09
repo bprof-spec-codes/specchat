a3a2a2b0a4b64edd6ef240c06f837aa35a5ea59d
Dávid <david.gyongyosi@majin.dev>
2023-05-09 10:33:14 +0200

    services added

--- ClientApp/src/app/services/message.service.ts
+++ ClientApp/src/app/services/message.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
-import { Chat } from '../models/chat';
+import { Message } from '../models/message';
 import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class ChatService {
-  private url = "api/Chat";
+export class MessageService {
+  private url = "api/Message";
   constructor(private http: HttpClient) { }
 
-  public getChat() : Observable<Chat[]> {
-    return this.http.get<Chat[]>(`${environment.baseApiUrl}/${this.url}`);
+  public getChat() : Observable<Message[]> {
+    return this.http.get<Message[]>(`${environment.baseApiUrl}/${this.url}`);
   }
 }
