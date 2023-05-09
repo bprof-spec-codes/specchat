0b9f8ae386a83c013150ed74b10ac9f54292e744
Dávid <david.gyongyosi@majin.dev>
2023-05-09 15:44:24 +0200

    thread selector implemented

--- ClientApp/src/app/chat-list/chat-list.component.ts
+++ ClientApp/src/app/chat-list/chat-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-chat-list',
   templateUrl: './chat-list.component.html',
--- ClientApp/src/app/thread-list/thread-list.component.ts
+++ ClientApp/src/app/thread-list/thread-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MessageService } from '../services/message.service';
 import { Message } from '../models/message';
+import { Chat } from '../models/chat';
 
 @Component({
   selector: 'app-thread-list',
@@ -8,14 +9,27 @@ import { Message } from '../models/message';
   styleUrls: ['./thread-list.component.css']
 })
 export class ThreadListComponent implements OnInit {
-  msgs?: Message[];
-  
-  constructor(private chatService: MessageService) {}
+  msgs: Message[] = [];
+  selectedChat!: Chat;
+
+  constructor(private messageService: MessageService) {}
+
+  @Input() set selectedThread(chat: Chat) {
+    this.selectedChat = chat;
+  }
 
   ngOnInit(): void {
-    this.chatService.getChat().subscribe((result: Message[]) => {
+    this.messageService.getChat().subscribe((result: Message[]) => {
       this.msgs = result;
-      console.log(result);
     });
+  }
+
+  showMsgs() {
+    if (!this.selectedChat) {
+      return [];
+    }
+    return this.msgs.filter(
+      (x) => x.chatId === this.selectedChat.id && x.MainMessageId == null
+    );
   }
 }
