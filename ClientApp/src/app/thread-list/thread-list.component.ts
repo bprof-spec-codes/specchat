fc6875e152de7b79aa127d1df6761a4c194b5b55
Dávid <david.gyongyosi@majin.dev>
2023-05-09 10:33:47 +0200

    thread list component created

---
+++ ClientApp/src/app/thread-list/thread-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../services/message.service';
+import { Message } from '../models/message';
+
+@Component({
+  selector: 'app-thread-list',
+  templateUrl: './thread-list.component.html',
+  styleUrls: ['./thread-list.component.css']
+})
+export class ThreadListComponent implements OnInit {
+  msgs?: Message[];
+
+  constructor(private chatService: MessageService) {}
+
+  ngOnInit(): void {
+    this.chatService.getChat().subscribe((result: Message[]) => {
+      this.msgs = result;
+      console.log(result);
+    });
+  }
+}
